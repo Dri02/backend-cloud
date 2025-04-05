1cef409f813942172263b51fad6878716875f6e4
Dri02 <108200161+Dri02@users.noreply.github.com>
2025-04-05 17:17:44 -0400

    Actualizar backend.js

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
+const morgan = require("morgan");
 const routes = require("./routes.js");
 
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: (origin, callback) => {
+      const allowedOrigins = ["http://localhost:3000", "http://localhost:8000", "http://localhost:8081"];
+      if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+      } else {
+          callback(new Error("No permitido por CORS"));
+      }
+  },
   })
 );
 
@@ -18,6 +26,7 @@ app.use(
   })
 );
 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
